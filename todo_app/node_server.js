55895d19adbaedd994e169dafbed9227d5076574
Sierra <sierrabrown808@gmail.com>
2014-11-16 20:26:45 -0800

    custom put request

--- todo_app/node_server.js
+++ todo_app/node_server.js
@@ -23,6 +23,41 @@ var server = http.createServer(function(req, res) {
 			res.setHeader('Content-Length', Buffer.byteLength(body));
 			res.setHeader('Content-Type', 'text/plain; charset="utf-8"');
 			res.end(body);
+	
+		case 'DELETE':
+			var path = url.parse(req.url).pathname;
+			var i = parseInt(path.slice(1), 10);
+			if (isNaN(i)) {
+				res.statusCode = 400;
+				res.end('Invalid item id.');
+			} else if (!items[i]) {
+				res.statusCode = 404;
+				res.end('Item not found');
+			} else {
+				items.splice(i, 1);
+				res.end('OK\n')
+			}
+		
+		case 'PUT':
+			var item = '';
+			var path = url.parse(req.url).pathname;
+			var i = parseInt(path.slice(1), 10)
+			req.setEncoding('utf8');
+			if (isNaN(i)) {
+				res.statusCode = 400;
+				res.end('Invalid item id.')
+			} else if (!items[i]) {
+				res.statusCode = 404;
+				res.end('Item not found')
+			} else {
+				req.on('data', function(chunk) {
+					item += chunk;
+				});
+				req.on('end', function() {
+					items[i] = item;
+					res.end('OK\n');
+				})
+			}
 	}
 });
 
